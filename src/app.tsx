cf321a802c8b70917c76e7ef896678d36235ca40
Bora Semiz <bora@ireckonu.com>
2019-11-08 16:08:56 +0100

    Initial

---
+++ src/app.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect
+} from 'react-router-dom';
+
+import { Navbar } from './navbar';
+import { Sidebar } from './sidebar';
+import { Widgets } from './pages/widgets';
+
+export default (): React.ReactElement => (
+  <Router>
+    <React.Fragment>
+
+      <Navbar />
+      <Sidebar />
+
+      <div className="content-wrapper">
+        <Route
+          exact path="/"
+          component={() => <Redirect to="/widgets" />}
+        />
+        <Route
+          path="/widgets"
+          component={Widgets}
+        />
+      </div>
+      
+    </React.Fragment>
+  </Router>
+);
