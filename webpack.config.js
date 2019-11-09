b49170ebc3f0e51b8430374001f7ef60cdc5939a
Bora Semiz <borasemiz50@gmail.com>
2019-11-09 17:04:38 +0100

    Webpack as the module bundler

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'index.ts'),
+  devtool: 'source-map',
+  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.bundle.js'
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        exclude: /node_modules/,
+        test: /\.tsx?$/,
+        use: 'ts-loader'
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'index.html'),
+    }),
+    new CopyWebpackPlugin([
+      {
+        from: 'assets',
+        to: path.resolve(__dirname, 'dist', 'assets')
+      }
+    ])
+  ],
+  devServer: {
+    compress: true,
+    port: 4204,
+    historyApiFallback: true
+  }
+};
